0a5623be7b394dd08355d7452e7108a69e175a1a
Juan <apolo931@outlook.es>
2023-02-25 18:00:55 -0700

    PDF Reporte (avance)

--- resources/views/PDF/reporteAlumno.blade.php
+++ resources/views/PDF/reporteAlumno.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Reporte PDF Generico</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <style>
         h3{
             background-color: blue;
@@ -14,17 +15,117 @@
             border: 1px solid black;
             border-radius: 20px;
         }
+
+        .contenedor{
+            display: flex;
+            
+        }
+
+        p{
+            font-size: 10px; 
+        }
+        .card{
+            display: flex;
+        }
+
+        .pie-pagina{
+            text-align: center;
+        }
+
+        .footer{
+            padding: 1em 0;
+            text-align: center;
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+        }
+
     </style>
 </head>
 <body>
-    <h1>Datos del alumno</h1><hr>
-    <h5>ID: {{ $alumno->id }}</h5>
-    <h5>Nombre: {{ $alumno->nombre }}</h5>
-    <h5>Edad: {{ $alumno->edad }}</h5>
-    @if ($alumno->sexo == 0)
-        <h5>Sexo: Femenino</h5>
-    @else
-        <h5>Sexo: Masculino</h5>
-    @endif
+
+
+
+
+   
+
+<div class="card">
+  <div class="card-header" style="background-color: #4b5050;  color: white; text-align: center">
+    Historial Académico
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+
+    <div>
+    
+
+            <p><b>Nombre del subsistema</b> <small>DIRECCIÓN GENERAL DE EDUCACIÓN TECNOLÓGICA INDUSTRIAL Y DE SERVICIOS</small></p>
+            <p><b>CURP:</b> <small>{{ $alumno-> CURP}}</small></p> 
+            <p><b>Nombre del alumno:</b> <small>{{ $alumno-> nombre_completo}}</small></p>
+            <p><b>Folio RNA:</b></p>
+            <p><b>Periodo Ingreso:</b></p>
+            <p><b>Avance creditos:</b></p>
+        
+        </div>
+
+
+
+            <p><b>Nombre del subsistema</b> <small>DIRECCIÓN GENERAL DE EDUCACIÓN TECNOLÓGICA INDUSTRIAL Y DE SERVICIOS</small></p>
+
+
+</div>
+    </div>
+    </blockquote>
+  </div>
+</div>
+
+    <br><br>
+     <div class="responsive-table ">
+    <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Nombre</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</div> 
+
+
+
+
+
+
+
+
+
+
+<div class="footer">
+    <p>MARTITZA DEL CARMEN CAZAREZ RIOS</p>
+    <p>_____________</p>
+    <p style="font-size: 8px">LA DIRECTORA DEL PLANTEL</p>
+</div>
 </body>
 </html>
