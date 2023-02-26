9e1bf7d6a953fc450937e330bd4d9fa307e55ac3
StephenOchoa <stephen.camargo@cetis107.edu.mx>
2023-02-25 19:20:42 -0700

    login y register V2

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 form-group col-6" style="background-color:#DDDDDD;">
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
         
-            
+            <img src="{{ url('images/DGETI.png') }}" width="400" height="400"><br>
                 {{ $slot }}
             </div>
             
